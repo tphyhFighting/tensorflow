c83cf34e55a846d3de56bbee762c8ceee4ecf37f
tphyhFighting <2363176358@qq.com>
2016-11-08 22:00:48 +0800

    python_ex1_4

---
+++ LearningPythonHard/ex3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+print "I will now count my chickens:"
+#計算25+30/6
+print "Hens",25+30.0/6
+print "Roosters",100-25.0*3.0%4
+print "Now I will count the eggs:"
+#python中的%代表取於
+print 3+2+1-5+4.0%2-1.0/4+6
+print "Is it true that 3+2<5-7?"
+print 3.0+2.0<5.0-7.0
+print "What is 3+2?",3.0+2.0
+print "What is 5-7?",5.0-7.0
+print "Oh ,that's why it's False"
+print "How about some more"
+print "Is it greater?",5.0>-2.0
+# >=標示大於等於
+print "Is it greater or equal?",5.0 >= -2.0
+print "Is it less or equal?",5.0 <= -2.0
