1dbcc95814f069ba955c20222b51a0d7a77a6c1f
tphyhFighting <2363176358@qq.com>
2016-11-11 18:03:23 +0800

    ex11-12

---
+++ LearningPythonHard/ex11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#raw_input():accept input,convert it to string and return  
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+print "So you are %r old,%r tall and %r weight."%(age,height,weight)
+
