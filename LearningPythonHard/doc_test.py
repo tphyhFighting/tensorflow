371889cb8430fa7b9e464bbbf050610bf7722b74
tphyhFighting <2363176358@qq.com>
2016-12-01 21:47:02 +0800

    ex46-47doctest

---
+++ LearningPythonHard/doc_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+def multiply(a,b):
+    '''
+    >>> multiply(2,3)
+    6
+    >>> multiply('baka~',3)
+    'baka~baka~baka~'
+    '''
+    return a*b
+#if __name__ == '__main__':
+    #import doctest
+    #doctest.testmod(verbose=True)
+
---
+++ LearningPythonHard/test/test.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+def multiply(a,b):
+	return a*b
