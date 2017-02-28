328093c3d1e1c11522752e6a8eb4f68dd8c2c680
tphyhFighting <2363176358@qq.com>
2017-02-28 17:24:01 +0800

    import,reload

---
+++ Learning Python 5E/module1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# --*-- coding:utf-8 --*--
+def printer(x):
+	print x
---
+++ Learning Python 5E/small.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# --*-- coding:utf-8 --*--
+#模块会在第一次import或from时载入并执行，并且只有第一次如此。
+x = 1
+y = [1, 2]
+#reload函数会强制已加载的模块的代码重新载入并重新执行。
