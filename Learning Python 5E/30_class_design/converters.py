bba93525341272903f3fa470a23ccaf9f32f7445
tphyhFighting <2363176358@qq.com>
2017-03-15 18:08:44 +0800

    30_类继承，组合，委托

---
+++ Learning Python 5E/30_class_design/converters.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# --*-- coding:utf-8 --*--
+from streams import Processor
+class Uppercase(Processor):
+	def converter(self, data):
+		return data.upper()
+if __name__ == '__main__':
+	import sys
+	obj = Uppercase(open('spam.txt'), sys.stdout)
+	obj.process()
---
+++ Learning Python 5E/30_class_design/pizzashop.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# --*-- coding:utf-8 --*--
+from employees import PizzaRobot, Server
+class Customer(object):
+	def __init__(self, name):
+		self.name = name
+	def order(self, server):
+		print(self.name, "orders from", server)
+	def pay(self, server):
+		print(self.name, "pays for item to", server)
+class Oven(object):
+	def bake(self):
+		print("oven bakes")
+class PizzaShop(object):
+	def __init__(self):
+		self.server = Server('Pat')
+		self.chef = PizzaRobot('Bob')
+		self.oven = Oven()
+	def order(self, name):
+		customer = Customer(name)
+		customer.order(self.server)
+		self.chef.work()
+		self.oven.bake()
+		customer.pay(self.server)
+if __name__ == "__main__":
+	scene = PizzaShop()
+	scene.order('Homer')
+	print('...')
+	scene.order('Shaggy')
