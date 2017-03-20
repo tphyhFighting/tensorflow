09cd75feb0ae918e05c25b09a2ce671e338f582f
tphyhFighting <2363176358@qq.com>
2017-03-20 21:45:21 +0800

    class extend

---
+++ Learning Python 5E/31_class_advanced_usage/typesubclass.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# --*-- coding:utf-8 --*--
+#Subclass built-in list type/class
+#Map 1..N to 0..N-1;call back to built-in version
+class MyList(list):
+	def __getitem__(self, offset):
+		print('(indexing %s at %s)'%(self, offset))
+		return list.__getitem__(self, offset - 1)
+if __name__ == '__main__':
+	print(list('abc'))
+	x = MyList('abc')
+	print(x)
+	
+	print(x[1])
+	print(x[3])
+
+	x.append('spam')
+	print(x)
+
+	x.reverse()
+	print(x)
