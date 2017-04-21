fc5eeb7396568ae50dbccef3769a9c3c30d9bcd9
tphyhFighting <2363176358@qq.com>
2017-04-21 21:23:07 +0800

    exception_design

---
+++ Learning Python 5E/35_exception_design/nestexc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# --*-- coding:utf-8 --*--
+def action2():
+	print(1 + [])
+def action1():
+	try:
+		action2()
+	except TypeError:
+		print('inner try')
+
+try:
+	action1()
+except TypeError:
+	print('outer try')
